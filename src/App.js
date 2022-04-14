53d0d12531b2af1bff9117ba7d08fe281954e4f1
Raghavender Reddy Lonka <96302103+Raghavender-lonka@users.noreply.github.com>
2022-04-14 11:06:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import "./App.css"
 import Form from "./components/Form"
 
 function App() {
-  const array = [
-    [
-      {
-        name: "raghu",
-        contact: 9898988,
-      },
-    ],
-  ]
+
   return (
     <div className="App">
       <Form />
