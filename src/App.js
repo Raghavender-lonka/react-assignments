8e33cabe81b567b5541d4b7b14ca89fea8f6cf2e
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-04-14 11:01:35 +0530

    react-hands-on-2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { useState } from "react"
+import React from "react"
 import "./App.css"
-import ClassComponent from "./components/ClassComponent"
-import Func from "./components/Func"
-import Header from "./components/Header"
+import Form from "./components/Form"
 
 function App() {
-  const [showFuncComponent, setShowFuncComponent] = useState(false)
-  const [showClassComponent, setShowClassComponent] = useState(false)
-
-  const showFuncHandler = () => {
-    setShowFuncComponent(!showFuncComponent)
-  }
-
-  const showClassHandler = () => {
-    setShowClassComponent(!showClassComponent)
-  }
-
+  const array = [
+    [
+      {
+        name: "raghu",
+        contact: 9898988,
+      },
+    ],
+  ]
   return (
     <div className="App">
-      <Header onshowFunc={showFuncHandler} onshowClass={showClassHandler} />
-      <div className="component-container">
-        {showFuncComponent ? <Func className="funcComp" /> : ""}
-        {showClassComponent ? <ClassComponent className="classComp" /> : ""}
-      </div>
+      <Form />
     </div>
   )
 }
---
+++ src/components/FormData.jsx
@@ -0,0 +1,24 @@
+// import React from "react"
+
+// function FormData(props) {
+//   return (
+//     <div>
+//       {props.onreceiveData.flat().map((info, index) => {
+//         {
+//           console.log(info)
+//         }
+//         return (
+//           <div key={index}>
+//             <h1>
+//               Name: {info.name} | Department: {info.dept} | Rating:{" "}
+//               {info.rating}
+//             </h1>
+//           </div>
+//         )
+//       })}
+//       <h1>Data</h1>
+//     </div>
+//   )
+// }
+
+// export default FormData
